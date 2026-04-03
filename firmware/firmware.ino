4574ddeb54fba3ffdb4a0d7365ef5aa1c423c3f7
jostsalathe <jostsalathe@gmail.com>
2026-04-03 13:44:37 +0200

    added Function struct and improved press timing and repeat handling The new Function struct helps to better organize function definitions and key pin association as well as reduce code duplication. There now exist separate times for debounce, time between press and second send as well as time between repeat sends after that.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -10,19 +10,38 @@
 
 #include <IRremote.hpp>
 
-const static int KEY_PWR = 4;
-const static int KEY_VOLUMEUP = A3;
-const static int KEY_VOLUMEDOWN = A0;
+// codes for Philips 32PFL5403D according to IRDB
+// https://github.com/probonopd/irdb/blob/master/codes/Philips/Unknown_32PFL5403D/0%2C-1.csv
+
+
+struct Function {
+  int keyPin;
+  uint8_t rc6Address;
+  uint8_t rc6Command;
+  const char* name;
+};
+
+static constexpr int TIME_DEBOUNCE_MS = 10;
+static constexpr int TIME_SINGLE_PRESS_MS = 500;
+static constexpr int TIME_REPEAT_PRESS_MS = 100;
+static constexpr int N_FUNCTIONS = 3;
+
+static constexpr Function functions[N_FUNCTIONS] = {
+  {4, 0, 12, "KEY_PWR"},
+  {A3, 0, 16, "KEY_VOLUMEUP"},
+  {A0, 0, 17, "KEY_VOLUMEDOWN"},
+};
+
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(IR_SEND_PIN, OUTPUT);
-  pinMode(KEY_PWR, INPUT_PULLUP);
-  pinMode(KEY_VOLUMEUP, INPUT_PULLUP);
-  pinMode(KEY_VOLUMEDOWN, INPUT_PULLUP);
+  for (auto fn : functions) {
+    pinMode(fn.keyPin, INPUT_PULLUP);
+  }
 
-  Serial.begin(115200);
+  Serial.begin(2000000);
 
   Serial.println();
   Serial.println();
@@ -32,26 +51,62 @@ void setup() {
   Serial.println();
 }
 
-int volume = 0;
-
 void loop() {
   // put your main code here, to run repeatedly:
-  // read buttons and send codes according to IRDB
-  // https://github.com/probonopd/irdb/blob/master/codes/Philips/Unknown_32PFL5403D/0%2C-1.csv
-  if (!digitalRead(KEY_PWR)) {
-    IrSender.sendRC6(0, 12, 1);
-    delay(50);
-  }
-  if (!digitalRead(KEY_VOLUMEUP)) {
-    IrSender.sendRC6(0, 16, 1);
-    Serial.print("Vol=");
-    Serial.println(++volume);
-    delay(50);
-  }
-  if (!digitalRead(KEY_VOLUMEDOWN)) {
-    IrSender.sendRC6(0, 17, 1);
-    Serial.print("Vol=");
-    Serial.println(--volume);
-    delay(50);
+  // read buttons and send codes defined above
+
+  for (auto fn : functions) {
+    if (!digitalRead(fn.keyPin)) {
+      // this functions key was pressed
+      auto timeNextSendMs = millis();
+      int sendCount = 0;
+
+      Serial.print(timeNextSendMs);
+      Serial.print(" pressed ");
+      Serial.print(fn.name);
+
+      // debounce
+      while (millis() - timeNextSendMs < TIME_DEBOUNCE_MS) {
+        if (digitalRead(fn.keyPin)) {
+          Serial.println(" - debounce abort");
+          return; // the key was released
+        }
+      }
+
+      // still pressed - commence action
+      Serial.print(" - sending ");
+      Serial.print(fn.rc6Command);
+
+      // send once
+      IrSender.sendRC6(fn.rc6Address, fn.rc6Command, 0);
+      ++sendCount;
+      Serial.print(".");
+      
+      // wait for single send release
+      timeNextSendMs += TIME_SINGLE_PRESS_MS;
+      while (millis() < timeNextSendMs) {
+        if (digitalRead(fn.keyPin)) {
+          Serial.println(" 1 time");
+          return; // the key was released
+        }
+      }
+
+      // enter repeat mode for as long as key continues to be pressed
+      while (true) {
+        IrSender.sendRC6(fn.rc6Address, fn.rc6Command, 0);
+        ++sendCount;
+        Serial.print(".");
+        timeNextSendMs += TIME_REPEAT_PRESS_MS;
+        while (millis() < timeNextSendMs) {
+          if (digitalRead(fn.keyPin)) {
+            Serial.print(" ");
+            Serial.print(sendCount);
+            Serial.println(" times");
+            Serial.flush();
+            return; // the key was released
+          }
+        }
+      }
+    }
   }
 }
