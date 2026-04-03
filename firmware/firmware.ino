743c0100e365f0ab810e8d3d98234467da228dc3
jostsalathe <jostsalathe@gmail.com>
2026-04-03 22:16:54 +0200

    renamed functions to commands added command definitions as comments in preparation for the ATtiny pcb

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -14,7 +14,7 @@
 // https://github.com/probonopd/irdb/blob/master/codes/Philips/Unknown_32PFL5403D/0%2C-1.csv
 
 
-struct Function {
+struct Command {
   int keyPin;
   uint8_t rc6Address;
   uint8_t rc6Command;
@@ -24,12 +24,23 @@ struct Function {
 static constexpr int TIME_DEBOUNCE_MS = 10;
 static constexpr int TIME_SINGLE_PRESS_MS = 500;
 static constexpr int TIME_REPEAT_PRESS_MS = 100;
-static constexpr int N_FUNCTIONS = 3;
 
-static constexpr Function functions[N_FUNCTIONS] = {
-  {4, 0, 12, "KEY_PWR"},
-  {A3, 0, 16, "KEY_VOLUMEUP"},
-  {A0, 0, 17, "KEY_VOLUMEDOWN"},
+static constexpr Command commands[] = {
+  // {Pin_PA2, 0, 0xff, "???"},
+  // {Pin_PA3, 0, 0xff, "???"},
+  {4, 0, 12, "KEY_PWR"}, // {Pin_PA4, 0, 12, "KEY_PWR"},
+  // {Pin_PA6, 0, 32, "KEY_CHANNELUP"},
+  {A3, 0, 16, "KEY_VOLUMEUP"}, // {Pin_PA7, 0, 16, "KEY_VOLUMEUP"},
+  // {Pin_PB0, 0, 245, "KEY_Picture_Format"},
+  // {Pin_PB1, 0, 33, "KEY_CHANNELDOWN"},
+  {A0, 0, 17, "KEY_VOLUMEDOWN"}, // {Pin_PB2, 0, 17, "KEY_VOLUMEDOWN"},
+  // {Pin_PB3, 0, 13, "KEY_MUTE"},
+  // {Pin_PB4, 0, 84, "KEY_MENU"},
+  // {Pin_PB5, 0, 88, "KEY_UP"},
+  // {Pin_PC0, 0, 92, "KEY_OK"},
+  // {Pin_PC1, 0, 90, "KEY_LEFT"},
+  // {Pin_PC2, 0, 89, "KEY_DOWN"},
+  // {Pin_PC3, 0, 91, "KEY_RIGHT"},
 };
 
 
@@ -37,8 +48,8 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(IR_SEND_PIN, OUTPUT);
-  for (auto fn : functions) {
-    pinMode(fn.keyPin, INPUT_PULLUP);
+  for (auto cmd : commands) {
+    pinMode(cmd.keyPin, INPUT_PULLUP);
   }
 
   Serial.begin(2000000);
@@ -55,19 +66,19 @@ void loop() {
   // put your main code here, to run repeatedly:
   // read buttons and send codes defined above
 
-  for (auto fn : functions) {
-    if (!digitalRead(fn.keyPin)) {
-      // this functions key was pressed
+  for (auto cmd : commands) {
+    if (!digitalRead(cmd.keyPin)) {
+      // this commands key was pressed
       auto timeNextSendMs = millis();
       int sendCount = 0;
 
       Serial.print(timeNextSendMs);
       Serial.print(" pressed ");
-      Serial.print(fn.name);
+      Serial.print(cmd.name);
 
       // debounce
       while (millis() - timeNextSendMs < TIME_DEBOUNCE_MS) {
-        if (digitalRead(fn.keyPin)) {
+        if (digitalRead(cmd.keyPin)) {
           Serial.println(" - debounce abort");
           return; // the key was released
         }
@@ -75,17 +86,17 @@ void loop() {
 
       // still pressed - commence action
       Serial.print(" - sending ");
-      Serial.print(fn.rc6Command);
+      Serial.print(cmd.rc6Command);
 
       // send once
-      IrSender.sendRC6(fn.rc6Address, fn.rc6Command, 0);
+      IrSender.sendRC6(cmd.rc6Address, cmd.rc6Command, 0);
       ++sendCount;
       Serial.print(".");
       
       // wait for single send release
       timeNextSendMs += TIME_SINGLE_PRESS_MS;
       while (millis() < timeNextSendMs) {
-        if (digitalRead(fn.keyPin)) {
+        if (digitalRead(cmd.keyPin)) {
           Serial.println(" 1 time");
           return; // the key was released
         }
@@ -93,12 +104,12 @@ void loop() {
 
       // enter repeat mode for as long as key continues to be pressed
       while (true) {
-        IrSender.sendRC6(fn.rc6Address, fn.rc6Command, 0);
+        IrSender.sendRC6(cmd.rc6Address, cmd.rc6Command, 0);
         ++sendCount;
         Serial.print(".");
         timeNextSendMs += TIME_REPEAT_PRESS_MS;
         while (millis() < timeNextSendMs) {
-          if (digitalRead(fn.keyPin)) {
+          if (digitalRead(cmd.keyPin)) {
             Serial.print(" ");
             Serial.print(sendCount);
             Serial.println(" times");
